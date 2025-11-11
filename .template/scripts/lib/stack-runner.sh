f863fd3eefcce6c7d02155a04ab23486ff684f84
Nick Nizovtsev <nizovtsevnv@gmail.com>
2025-11-11 18:31:33 +0400

    fix: использовать eval с одинарными кавычками для команды контейнера
    Проблема:
    При выполнении команд в контейнере возникала ошибка syntax error из-за
    неправильного экранирования команды в sh -c.
    
    Решение:
    Используем eval с одинарными кавычками вокруг $cmd чтобы избежать
    преждевременной интерпретации спецсимволов на хосте.
    
    Изменения:
    - Заменили прямой вызов на eval
    - Используем одинарные кавычки для sh -c '$cmd'
    - Экранированы все переменные внутри eval

--- .template/scripts/lib/stack-runner.sh
+++ .template/scripts/lib/stack-runner.sh
@@ -107,15 +107,17 @@ _run_stack_generic() {
 			;;
 	esac
 
+	# Выполняем команду в контейнере
+	# Используем eval для правильной интерпретации кавычек и экранирования
 	# shellcheck disable=SC2086
-	$CONTAINER_RUNTIME run --rm \
-		-v "$workspace_root:/workspace" \
+	eval "$CONTAINER_RUNTIME run --rm \
+		-v \"$workspace_root:/workspace\" \
 		$extra_mounts \
-		-w "$workdir_abs" \
-		-e "HOST_UID=$(id -u)" \
-		-e "HOST_GID=$(id -g)" \
-		"$container_image" \
-		/bin/sh -c "$cmd"
+		-w \"$workdir_abs\" \
+		-e \"HOST_UID=\$(id -u)\" \
+		-e \"HOST_GID=\$(id -g)\" \
+		\"$container_image\" \
+		sh -c '$cmd'"
 }
 
 # ===================================
